0b737efa0fd50caf94255454f5075c4c7bba7524
michael jones <michael.jowen.jones@gmail.com>
2021-08-31 16:24:16 +0100

    sus

--- src/1-async-return.ts
+++ src/1-async-return.ts
@@ -2,11 +2,12 @@ function greet(): void {
   console.log("hello world"!);
 }
 
-async function promiseToGreet(): Promise<void> {
+async function promiseToGreet(): Promise<string> {
   console.log("hello world"!);
+  return 'hello';
 }
 
-greet();
+// greet();
 // promiseToGreet()
 // greet().then(() => console.log("CANNOT do this"));
-// promiseToGreet().then(() => console.log("CAN do this"));
+promiseToGreet().then(() => console.log("CAN do this"));
